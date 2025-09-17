cba1295dad9021b100ca0a5d6edd57a97a2fafad
Abhishek Bhat <abhishekbhat@rosettesmartlife.onmicrosoft.com>
2025-09-17 17:54:01 +0530

    fifth commit

--- src/index.js
+++ src/index.js
@@ -13,8 +13,14 @@ const dashboardRoutes = require('./routes/dashboardRoutes');
 const gAuth = require('./routes/gAuthRoutes');
 
 const app = express();
-app.set('trust proxy', true);
+app.set('trust proxy', 1);
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+  message: "Too many requests from this IP, please try again later.",
+});
 
+app.use(limiter);
 // Test database connection on startup
 testConnection();
 
